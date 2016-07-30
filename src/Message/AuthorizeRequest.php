e7ac0b4af1c0988e4f9cf0bcc03cbbfab07b4419
Del <del@babel.com.au>
2016-07-30 15:32:51 +0700

    Fixes #5

--- src/Message/AuthorizeRequest.php
+++ src/Message/AuthorizeRequest.php
@@ -231,6 +231,7 @@ class AuthorizeRequest extends AbstractRequest
 
         $data['TENDER'] = 'C';
         $data['AMT'] = $this->getAmount();
+        $data['CURRENCY'] = $this->getCurrency();
         $data['COMMENT1'] = $this->getDescription();
         $data['COMMENT2'] = $this->getComment2();
         $data['ORDERID'] = $this->getTransactionId();
